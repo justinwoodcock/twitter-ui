dc55caeab259c0f53c6ffe1f3a28ffe066e5d12a
Justin Woodcock <justin@digitaluprisingdesign.com>
2014-08-07 00:12:24 -0700

    update hostname to match hosts entry

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function(grunt) {
             options: {
                 port: 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'ui-base.dev',
+                hostname: 'twitter.dev',
                 livereload: 35729
             },
             livereload: {
