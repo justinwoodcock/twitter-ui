2898f99f1b0d596ceebc198ade61ade62b9491b9
Justin Woodcock <justin@digitaluprisingdesign.com>
2014-08-12 07:48:03 -0700

    make the tweets look better & add tweet to sidebar element

--- app/components/_ui-elements/ui-elements.controller.js
+++ app/components/_ui-elements/ui-elements.controller.js
@@ -6,5 +6,25 @@ twitter.controller('UiElementsController', ['$scope', '$http', 'Restangular',
         // Users.getList().then(function(users){
         //     console.log(users);
         // })
+        
+        $scope.tweets = [
+        {
+            user: {
+                name: 'Justin Woodcock',
+                handle: '@justinkwoodcock',
+                avatar: 'http://placehold.it/48x48'
+            },
+            date: '1h',
+            content: '140 characters or less'
+        },
+        {
+            user: {
+                name: 'Chris Moore',
+                handle: '@camdesigns',
+                avatar: 'http://placehold.it/48x48'
+            },
+            date: '1h',
+            content: '140 characters or less'
+        }]
     }
 ]);
