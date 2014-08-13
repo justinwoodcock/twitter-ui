63c4bb4070708382af7d9356273e417ba675010a
Justin Woodcock <jwoodcock@undergroundelephant.com>
2014-08-13 01:46:24 -0700

    work on user view

--- app/app.js
+++ app/app.js
@@ -8,8 +8,16 @@ var twitter = angular.module('twitter', [
 
 twitter.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
     $httpProvider.defaults.useXDomain = true;
-    $stateProvider.state('user', {
-        url: '/user',
+    $stateProvider.state('login', {
+        url: '/login',
+        views: {
+            content: {
+                templateUrl: '/components/login/index.html',
+                controller: 'LoginController'
+            }
+        }
+    }).state('user', {
+        url: '/user/:handle',
         views: {
             content: {
                 templateUrl: '/components/user/index.html',
@@ -27,4 +35,17 @@ twitter.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
     });
 
     $urlRouterProvider.otherwise('/ui-elements');
+});
+
+twitter.config(function(RestangularProvider) {
+    RestangularProvider.setBaseUrl('http://mock.twitter.dev');
+    RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+        var extractedData;
+        if (operation === 'getList') {
+            extractedData = data.data;
+        } else {
+            extractedData = data.data;
+        }
+        return extractedData;
+    });
 });
--- app/components/user/user.controller.js
+++ app/components/user/user.controller.js
@@ -1,7 +1,35 @@
 'use strict';
 
-twitter.controller('UserController', ['$scope',
-    function($scope) {
-        
+twitter.controller('UserController', ['$scope', 'Restangular', '$stateParams',
+
+    function($scope, Restangular, $stateParams) {
+
+        var Users = Restangular.all('users');
+        Users.getList().then(function(users) {
+            $scope.users = users.plain();
+            $scope.usersEntity = users;
+            console.log($scope.users);
+
+            $scope.user = getUser($stateParams.handle);
+            $scope.tweets = getTweets($scope.user.id);
+
+        })
+
+        function getUser(handle) {
+            for (var i = 0; $scope.users.length > i; i++) {
+                if ($scope.users[i].handle === handle) {
+                    return $scope.users[i];
+                }
+            }
+        }
+
+        function getTweets(userId) {
+            // var Tweets = Restangular.one('users/').one(userId + '/').one('tweets');
+            // Tweets.getList().then(function(data) {
+            //     console.log(data);
+            //     return data;
+            // })
+        }
+
     }
 ]);
